e194b60a49bd88222816afe3dc1a7eceab422625
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-10-10 18:31:00 -0500

    add useState

--- src/components/Miniatures.js
+++ src/components/Miniatures.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function Miniatures() {
+    const [ miniatures, setMiniatures ] = useState([]);
+
     useEffect (() => {
         fetch('http://localhost:9292/miniatures')
         .then(res => res.json())
-        .then(data => {
-            console.log(data)
+        .then(data => setMiniatures(data))
         })
-    })
 
     return (
         <p></p>
--- src/components/Sets.js
+++ src/components/Sets.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function Sets() {
+    const [ sets, setSets ] = useState([]);
+
     useEffect (() => {
         fetch('http://localhost:9292/miniature_sets')
         .then(res => res.json())
-        .then(data => {
-            console.log(data)
+        .then(data => setSets(data))
         })
-    })
+    
 
     return (
         <p></p>
