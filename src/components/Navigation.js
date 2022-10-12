2f66feac00e42c5b1fd94eb5555e44e73be8d2b3
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-10-12 17:44:43 -0500

    add active route css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ function Navigation() {
     return (
         <nav>
             <div className="site-title">MINIATURE TRACKER</div>
-            <NavLink to="/">HOME</NavLink>
+            <NavLink to="/" end>HOME</NavLink>
             <NavLink to="/sets">SETS</NavLink>
             <NavLink to="/miniatures">MINIATURES</NavLink>
         </nav>
