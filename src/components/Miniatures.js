7b103bba5740d1e07df314b43ca36bc3536b3b5a
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-01-17 23:37:33 -0600

    add miniature sorting by oldest and latest

--- src/components/Miniatures.js
+++ src/components/Miniatures.js
@@ -1,19 +1,3 @@
-// import React from "react";
-// import MiniatureCard from "./MiniatureCard";
-
-// function Miniatures({ miniatures }) {
-//     const miniatureCards = () => miniatures.map((miniature, index) => <MiniatureCard key={index} miniature={miniature}/>)
-
-//     return (
-//         <div>
-//             <div className="mini-header">
-//                 <h3 className="counter">You have {miniatures.length} unique miniatures in your collection.</h3>
-//             </div>
-//             <div className="card-grid">{miniatureCards()}</div>
-//         </div>
-//     )
-// }
-
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MiniatureCard from "./MiniatureCard";
